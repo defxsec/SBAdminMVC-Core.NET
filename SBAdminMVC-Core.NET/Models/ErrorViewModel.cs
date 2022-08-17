e20c723a88cb1433cc53a000de970b5032c308f8
defxsec <defxsec@hotmail.com>
2022-08-17 15:26:44 -0400

    Agregar archivos de proyecto.

---
+++ SBAdminMVC-Core.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SBAdminMVC_Core.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
